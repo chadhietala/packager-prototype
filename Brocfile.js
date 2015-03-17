401d47080b535cb43daf8cf06c0c8c8b025ee032
Chad Hietala <chadhietala@gmail.com>
2015-03-17 16:02:20 -0700

    Creating dep-graph.json

--- Brocfile.js
+++ Brocfile.js
@@ -13,17 +13,12 @@ var rename = stew.rename;
 var app = 'app';
 app = find(app, '**/*.js');
 
-app = new ES6Modules(app, {
-  esperantoOptions: {
-    absolutePaths: true,
-    strict: true
-  }
-});
-
 var moment = 'node_modules/ember-moment';
 var momentApp = rename(find(moment, 'app/**/*.js'), function(relativePath) {
   return relativePath.replace('node_modules/ember-moment/app', 'app');
 });
+
+// Make this dynamic
 var momentAddon = rename(find(moment, 'addon/**/*.js'), function(relativePath) {
   return relativePath.replace('node_modules/ember-moment/addon', 'ember-moment');
 });
@@ -32,11 +27,34 @@ app = es6Transpiler(app, {
   blacklist: ['useStrict', 'es6.modules']
 });
 
+app = mergeTrees([momentApp, app], {overwrite: true});
 
 
-app = mergeTrees([app, momentApp], {overwrite: true});
-app = mergeTrees([app, momentAddon]);
+
+app = rename(mergeTrees([app, momentAddon]), 'app/', 'packager-proto/');
+
+app = new ES6Modules(app, {
+  esperantoOptions: {
+    absolutePaths: true,
+    strict: true
+  }
+});
+
+// dist/~my-app
+
+// appDependencies = withDependencies(app, {
+
+// });
+
+// dist/~my-app
+// dist/ember-moment/addon/...
+// dist/dependency-graph.json
+
+
 app = log(app);
+
+
+
 
 // app = concat(app, {
 //   inputFiles: ['**/*.js'],
